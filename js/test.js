34641be6f3f3a14ec7bd0be25534888568494b27
hello2tothe5 <hello2tothe5@gmail.com>
2026-04-28 18:05:11 +0900

    4월 28일 오후 6시 5분

--- js/test.js
+++ js/test.js
@@ -1,24 +1,35 @@
-<<<<<<< HEAD
-// 현재시간을 출력하기 위하여 우선 현재 시간을 구해
-// 오전 오후
-let date = new Date();
-let hour = date.getHours();
-if(hour < 5) {
-    alert('잠');
-} else {
-    if (hour < 7) {
-        alert('깸');
+// let i = 1;
+// let tot = 0;
+// while(i<=10); {
+// tot = tot +i;
+// i++;
+// }
+// console.log(tot);
+
+// let i = 1;
+// while(1) {
+//     console.log("Welcome");
+//     if (i === 7) {
+//         break;
+//     }
+//     i++;
+// }
+
+
+// 오늘 배운 거 총 정리 ↓↓↓
+
+ while (1) {
+     let input = parseInt(prompt("정수 입력"));
+     if(input === 7) {
+         console.log('7이 입력되었으니 종료');
+         break;
+     }
+     let tot = 0;
+     for(i=1; i <= input; i++){
+        tot = tot + i;
+     }
+     console.log(tot);
     }
-} else {
-    if (7 <= hour < 9) {
-        alert('일');
-    }
-} else {
-    if (9<= hour < 12) {
-        alert('밥');
-    }
-} else {
-    if (12 <= hour < 4) {
-        alert('일'); 
-    }
-}
+
+// 사용자로부터 입력받은 정수까지의 정수의 합계를 출력하시오
+
