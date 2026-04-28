b5cd78d5955a1e1ad14ad3098770c18f69179be2
hello2tothe5 <hello2tothe5@gmail.com>
2026-04-28 12:49:28 +0900

    4월 28일 if 중첩

--- js/test.js
+++ js/test.js
@@ -1,6 +1,23 @@
-let num = 10;
-if(num % 2 === 1) {
-    console.log("num은 홀수이다!!");
+// 현재시간을 출력하기 위하여 우선 현재 시간을 구해
+// 오전 오후
+let date = new Date();
+let hour = date.getHours();
+if(hour < 5) {
+    alert('잠');
 } else {
-    console.log("num은 짝수이다!!");
+    if (hour < 7) {
+        alert('깸');
+    }
+} else {
+    if (7 <= hour < 9) {
+        alert('일');
+    }
+} else {
+    if (9<= hour < 12) {
+        alert('밥');
+    }
+} else {
+    if (12 <= hour < 4) {
+        alert('일'); 
+    }
 }
